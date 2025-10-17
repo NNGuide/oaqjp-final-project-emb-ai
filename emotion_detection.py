388e1039b080913557ac098eba4831798d1c8f68
NNGuide <nati9112.na@gmail.com>
2025-10-17 19:36:50 -0400

    task_2

---
+++ emotion_detection.py
@@ -0,0 +1,14 @@
+import requests
+
+def emotion_detector(text_to_analyse):
+
+    # Input parameters
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    text_json = { "raw_document": { "text": text_to_analyse } }
+    
+    # Send a POST request to the Watson NLP library
+    response = requests.post(url, json = text_json, headers = header)
+
+    # return the response text from the library
+    return response.text
